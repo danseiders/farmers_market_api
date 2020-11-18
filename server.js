65c4558993b18b8064de6fa8ecc150c215be01da
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-11-17 20:17:50 -0600

    Database connection functional

--- server.js
+++ server.js
@@ -3,28 +3,32 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-
 // Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
-
-<<<<<<< HEAD
-//DATABASE
 const MONGODB_URI = process.env.MONGODB_URI
+const mongoConnection = `${MONGODB_URI}/farmStan`
 
 //MIDDLEWARE
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-=======
+//DATABASE
+mongoose.connect(mongoConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}, () => {
+  console.log(`Connected to MONGODB at ${mongoConnection}`)
+})
+
 // Controllers
 const farmController = require('./controllers/farm.js')
 app.use('/farm', farmController)
 
 const userController = require('./controllers/users.js')
 app.use('/users', userController)
->>>>>>> c2b456fe4c2603e92b4aca62430b2665614a5a8f
 
 app.get('/', (req, res) => {
   res.send('🎉')
