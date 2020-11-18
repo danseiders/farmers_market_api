5b56f3ff459b8648402be085d9b2e3f3921804a7
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-11-17 20:41:54 -0600

    New user create route

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,28 @@
+// Dependencies
 const express = require('express')
-
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const users = express.Router()
+const User = require('../models/user.js')
+
 
 users.get('/', (req, res) => {
   res.status(200).send({
     message: 'user info goes here'
+  })
+})
+
+
+users.post('/new', (req, res) => {
+  User.find({ username: req.body.username }, (err, foundUser) => {
+    if (foundUser.length > 0) {
+      res.status(400).json({ error: 'User already exists' })
+    } else {
+      req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+      User.create(req.body, (err, createdUser) => {
+        res.status(200).json({ newUser: createdUser })
+      })
+    }
   })
 })
 
