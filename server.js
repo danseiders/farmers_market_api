9f18bb2220d22d1a1ce62e97d0e46cd38340b4ac
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-11-17 21:36:50 -0600

    Updated mongo connection to work with MongoDB Atlas instead of local database

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = express()
 // Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
-const mongoConnection = `${MONGODB_URI}/farmStan`
+const mongoConnection = process.env.MONGODB_URI
 
 //MIDDLEWARE
 app.use(express.static('public'))
